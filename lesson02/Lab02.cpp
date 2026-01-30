00e35b652166c9291ba92827bc9c4ff3188701c2
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-01-30 13:16:11 -0500

    Update Lab02.cpp

--- lesson02/Lab02.cpp
+++ lesson02/Lab02.cpp
@@ -7,41 +7,46 @@
    - Display the solution to the problem
 */
 
-#include <iostream>  // Allows use of input/output streams (cout, cin)
-#include <iomanip>   // Allows use of formatting tools (fixed, setprecision)
-#include <limits>    // Allows use of properties and bounds (numeric_limits)
-using namespace std; // Allows use of standard library without std:: prefix
+#include <iostream>   // Allows use of input/output streams (cout, cin)
+#include <cstdlib>    // Allows use of random generators (rand, srand)
+#include <ctime>      // Allows use of seeding rand (time)
+#include <iomanip>    // Allows use of (max)
+#include <string>     // Allows use of string (getline, string)
 
-int main() {  // Program execution starts here
-  // Declare constants
-  const double TAX_RATE = 0.0675; // 6.75%
-  const double TIP_RATE = 0.20; // 20%
+using namespace std;  // Allows use of standard library w/out std:: prefix
+
+int main() {          // Define Method 'main'
+   // Seed random number generator once per run
+   srand(static_cast<unsigned>(time(nullptr)));   // Makes numbers different
    
-  // Declare and initialize variables
-  double charge = 0.0, tax = 0.0, subtotal = 0.0, tip = 0.0, total = 0.0;
-  
-  // Get user input and validate if charge is numeric and positive
-  while (true) { // While loop repeats until we break
-     cout << "Enter Price of Meal: $";                    // Get user input
-     if (cin >> charge && charge >= 0.0) break;           // If loop to validate if input is numeric AND positive
-     
-     cout << "ERROR: Invalid Input" << endl;              // ERROR Message for invalid input
-     cin.clear();                                         // Clear the fail state flag
-     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the rest of the line (the bad input)
-  }
+   // Generate two random numbers
+   int num1 = rand() % 100 + 1;   // Yeilds values from 1 to 100 (inclusive)
+   int num2 = rand() % 100 + 1;
 
-  // Calculate prices
-  tax = charge * TAX_RATE; // (6.75% of charge)
-  subtotal = tax + charge;
-  tip = subtotal * TIP_RATE; // (20% of subtotal)
-  total = subtotal + tip;
+   // Calculate sum
+   int sum = num1 + num2;
+      
+   // Format output by switching places
+   if (num1 < num2) {
+      swap (num1, num2);
+   }
 
-  // Output (formatted to 2 decimal places)
-  cout << fixed << setprecision(2);
-  cout << "  " << num1 << endl;
-  cout << "+ " << num2 << endl;
-  cout << "-----" << endl;
-  cout << "  " << total << endl;
-  
-  return 0;  // Ends the program
+   // Format output so numbers are right aligned w/width of numbers
+   int width = max({
+      (int)to_string(num1).size(),
+      (int)to_string(num2).size(),
+      (int)to_string(sum).size()
+   });
+   
+   // Get user input
+   cout << "  " << setw(width) << num1 << endl;
+   cout << "+ " << setw(width) << num2 << endl;
+   cout << string(width + 2, '-');
+   string pause;
+   getline(cin, pause);   // Wait for Enter w/out newline
+
+   // Display output
+   cout << " " << setw(width) << sum << endl;
+   
+   return 0; // End program
 }
