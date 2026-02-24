a496755ee7b0b88fb49de4f9104847843a3ae9da
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-23 19:31:50 -0500

    Create ArrayExample.cpp

---
+++ lesson06/ArrayExample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+
+int main {
+  // declare array
+  array<int, 10> myarray;
+  for (int i = 0; i < 10; i++) {
+    myarray.at(i) = i + 1;
+  }
+  
+  // print content
+
+}
