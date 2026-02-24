468bf4f8959bc6116444c3e0bdf0b20f1f9cb741
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-23 19:42:56 -0500

    Update VectorSwap.cpp

--- lesson06/VectorSwap.cpp
+++ lesson06/VectorSwap.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 int main() {
-  vector<int> ONE (3,100); // 3 ints with a value of 100
-  vector<int> TWO (5,200); // 5 ints with a value of 200 
+  vector<int> ONE (3, 100); // 3 ints with a value of 100
+  vector<int> TWO (5, 200); // 5 ints with a value of 200 
 
   ONE.swap(TWO);
 
   cout << "ONE contains:";
   for (unsigned i = 0; i <ONE.size(); i++)
     cout << ' ' << ONE[i];
+  cout << "\n";
 
   cout << "TWO contains:";
   for (unsigned i = 0; i <TWO.size(); i++)
