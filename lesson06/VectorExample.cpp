cb7148afbafb4de96f9f5832729eb5804ac061d4
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-23 19:33:31 -0500

    Create VectorExample.cpp

---
+++ lesson06/VectorExample.cpp
@@ -0,0 +1,11 @@
+int main
+vector<int> myvector(10);
+
+for (unsigned i = 0; i<myvector.size()>; i++)
+  my vector.at(i) = 2 * i;
+
+cout << "myvector contains: ";
+
+// Print only even numbers
+for (unsigned i = 0; i<myvector.size()>; i++)
+  my vector.at(i) = 2 * i;
