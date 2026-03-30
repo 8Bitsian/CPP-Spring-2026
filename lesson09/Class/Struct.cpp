d8e2e3f754e1a24aa4c53ae1c744f3306b931292
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-30 18:41:05 -0400

    Update Struct.cpp

--- lesson09/Class/Struct.cpp
+++ lesson09/Class/Struct.cpp
@@ -1,10 +1,22 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
-struct DataTypeName {
+struct Node {
+  int z;
+  struct Node next;
+  string str;
+  char ch[20];
 };
 
+/*
+function1();
+fucntion2();
+...
+*/
+
 int main() {
+  Node x;
   cout << "Hello World!";
   return 0;
 }
