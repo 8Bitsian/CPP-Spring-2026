849388c15006b8f54e47cd5bddab04a780e9072f
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-30 20:10:46 -0400

    Add CityInformation.cpp to manage city data

---
+++ lesson09/Class/CityInformation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct CityInfo {
+  string cityName;
+  string state;
+  long population;
+  int distance;
+};
+
+int main() {
+  A[0].cityName = "Nashville";
+  A[0].state = "TN";
+  A[0].population = "187564002";
+  A[0].distance = "1524";
+  
+  for (int i = 0; i < A[i] )
+  cout << A[i] << endl;
+  
+  return 0;
+}
