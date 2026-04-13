73a0dfcf99d43e0bd040bf1e45bf03dd0f2468cd
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-13 18:23:56 -0400

    Create ASCII.cpp

---
+++ Interview1/ASCII.cpp
@@ -0,0 +1,20 @@
+/*
+  Imani Hollie 04/13/2026
+  Menu-Drivn Progam to Display ASII Values
+  Directions: Create a menu-driven program that allows the user to select between displaying uppercase letters, lowercase letters, or xiting the program
+  The program must be able to:
+  - Display the vfollowing menu:
+    - 1. Uppercase Letter (Call DisplayUppercaseASCII() to display uppercase letters (A-Z) with ASCII values
+    - 2. Lowercase Letter (Call DisplayLowercaseASCII() to display lowercase letters (a-z) with ASCII values
+    - 3. EXIT PROGRAM (Terminate the program)
+  - The user must select an option from the menu
+  - The program must continue until the user slects the EXIT option
+  - Each function must display one letter and its ASCII value per line
+
+  Sample Output:
+  A = 65
+  B = 66
+  ...
+  Z = 90
+*/
+
