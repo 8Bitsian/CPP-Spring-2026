7ebd3c9eb4e1cbbd3a679e5aec7254e72f838f8d
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-13 20:05:34 -0400

    Create final.cpp

---
+++ final/final.cpp
@@ -0,0 +1,6 @@
+/*
+  Imani Hollie 04/26/2026
+  Homeowner Expenses Calculator
+  Directions: Create a modular, file-driven expense management system in C++.
+  The program will help homeowners track, analyze, and report on monthly expenses throughout the year.
+*/
